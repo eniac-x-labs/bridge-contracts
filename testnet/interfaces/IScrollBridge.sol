538db1a166148016d9d192941d0c05583cf92d39
supredu <1020665094@qq.com>
2024-03-03 13:01:58 +0800

    change iscrollbridge

--- testnet/interfaces/IScrollBridge.sol
+++ testnet/interfaces/IScrollBridge.sol
@@ -2,13 +2,13 @@
 pragma solidity 0.8.20;
 
 // https://scrollscan.com/address/0xe0a0509a66c509f55c85a20eb8c60676135081f7#code#F8#L47
-//interface IScrollStandardL2Bridge {
-//    function withdrawETH(
-//        address _to,
-//        uint256 _amount,
-//        uint256 _gasLimit
-//    ) external payable;
-//}
+interface IScrollStandardL2ETHBridge {
+   function withdrawETH(
+       address _to,
+       uint256 _amount,
+       uint256 _gasLimit
+   ) external payable;
+}
 
 interface IScrollStandardL2WETHBridge {
     function withdrawERC20(

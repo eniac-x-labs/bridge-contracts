e3302defcf8c9f49c4d2835bbf46b8380aa99f32
supredu <1020665094@qq.com>
2024-03-15 12:27:17 +0800

    add faucettoken for test

---
+++ testnet/Mock/faucetERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract faucetToken is ERC20 {
+    mapping(address => bool) public isClaimed;
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC20(name_, symbol_) {
+        _mint(_msgSender(), 1_000_000_000 * (10 ** decimals()));
+    }
+
+    function faucet() external {
+        require(isClaimed[msg.sender] == false, "Already Claimed");
+        isClaimed[msg.sender] = true;
+        _mint(msg.sender, 1);
+    }
+}
